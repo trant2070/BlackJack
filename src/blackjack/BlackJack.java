5577fc0047104769c82d2a46646cebc73e555f31
trant2070 <31450029+trant2070@users.noreply.github.com>
2019-04-06 08:55:30 -0700

    Update BlackJack.java

--- src/blackjack/BlackJack.java
+++ src/blackjack/BlackJack.java
@@ -6,34 +6,102 @@ package blackjack;
 //Class -
 //Lab  - 
 
+
 import static java.lang.System.*;
 import java.util.Scanner;
 
-public class BlackJack
-{
-	//add in Player instance variable
-	//add in Dealer instance variable
+public class BlackJack {
+	private Player player;
+        private Dealer dealer;
+        private char choice = 'n';
+        private char continuePlaying = 'n';
+        private int playerHandValue = 0;
+        private boolean busted = false;
 
-	public BlackJack()
-	{
-
-
+	public BlackJack() {
+            player = new Player();
+            dealer = new Dealer();
 	}
 
-	public void playGame()
-	{
+	public void playGame() {
 		Scanner keyboard = new Scanner(System.in);
-		char choice = 0;
-
-
-
-
-
+                
+                dealer.shuffle();
+             
+                do{
+                    busted = false;
+                    
+                    //Deal the player and dealer two cards
+                    player.addCardToHand(dealer.deal());
+                    player.addCardToHand(dealer.deal());
+                    dealer.addCardToHand(dealer.deal());
+                    dealer.addCardToHand(dealer.deal());
+                    
+                    out.println(player);
+                    
+                    //Player risks his money...
+                    playerHandValue = player.getHandValue();
+                    out.print("\nDo you want to hit? (Y/N) ");
+                    choice = keyboard.next().charAt(0);
+                    
+                    while (!busted && (choice == 'Y' || choice == 'y')){
+                        player.addCardToHand(dealer.deal());
+                        busted = (player.getHandValue() > 21);
+                        out.println(player);
+                        if (!busted){
+                            out.print("\n\nDo you want to hit? (Y/N) ");
+                            choice = keyboard.next().charAt(0);
+                        }
+                    }
+                    
+                    //Dealer risks his money...
+                    out.println("\nDEALER\n" + dealer.toString() + '\n');
+                    while (dealer.hit()){
+                        dealer.addCardToHand(dealer.deal());
+                        out.println(dealer);
+                    }
+                    
+                    //Determine which player won
+                    if (busted && dealer.getHandValue() <= 21){
+                        out.println("\nDealer wins! Player busted!");
+                        dealer.setWinCount(dealer.getWinCount() + 1);
+                    }
+                    else if (player.getHandValue() <= 21 && dealer.getHandValue() > 21){
+                        out.println("\nPlayer wins! Dealer busted!");
+                        player.setWinCount(player.getWinCount() + 1);
+                    }
+                    else if (player.getHandValue() > dealer.getHandValue() && player.getHandValue() <= 21){
+                        out.println("\nPlayer's hand is higher than dealer's!");
+                        player.setWinCount(player.getWinCount() + 1);
+                    }
+                    else if (player.getHandValue() < dealer.getHandValue() && dealer.getHandValue() <= 21){
+                        out.println("\nDealer's hand is higher than player's!");
+                        dealer.setWinCount(dealer.getWinCount() + 1);
+                    }
+                    else if (busted && dealer.getHandValue() > 21){
+                        out.println("\nPush...Nobody won.");
+                    }
+                    else{
+                        out.println("\nPush...Nobody won.");
+                    }
+                    
+                    out.println("\nDealer has won " + dealer.getWinCount() + " times.");
+                    out.println("Player has won " + player.getWinCount() + " times.");
+                    
+                    dealer.shuffle();
+                    
+                    out.print("\nWould you like to play again? (Y/N) ");
+                    continuePlaying = keyboard.next().charAt(0);
+                    out.println("\n");
+                    player.resetHand();
+                    dealer.resetHand();
+                    
+                } while (continuePlaying == 'Y' || continuePlaying == 'y');
 	}
 	
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		BlackJack game = new BlackJack();
 		game.playGame();
+        
 	}
 }
