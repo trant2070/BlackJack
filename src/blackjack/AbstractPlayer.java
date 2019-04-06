c41827b9a7258186a660cf2f20f07e01ed9eb7f6
trant2070 <31450029+trant2070@users.noreply.github.com>
2019-04-06 08:51:23 -0700

    Update AbstractPlayer.java

--- src/blackjack/AbstractPlayer.java
+++ src/blackjack/AbstractPlayer.java
@@ -6,54 +6,62 @@ package blackjack;
 //Class -
 //Lab  - 
 
-import java.util.ArrayList;
 import BlackJack.Card;
+import java.util.ArrayList;
 
-public abstract class AbstractPlayer implements Playerable
-{
-   private ArrayList<Card> hand;
+public abstract class AbstractPlayer implements Playerable {
+   private ArrayList<Card> hand = new ArrayList<Card>();
    private int winCount;
+   private ArrayList<Integer> skip = new ArrayList<Integer>();
 
-   //constructors
-
-   public  void addCardToHand( Card temp )
-   {
-
-
+   public AbstractPlayer() {
+       winCount = 0;
+   }
+   
+   public AbstractPlayer(ArrayList<Card> h, int w) {
+       hand = h;
+       winCount = w;
    }
 
-   public  void resetHand( )
-   {
-
-
+   public void addCardToHand(Card temp) {
+       hand.add(temp);
    }
 
-   public  void setWinCount( int numwins )
-   {
-
-
+   public void resetHand() {
+       hand = new ArrayList<Card>();
    }
 
-   public int getWinCount() { return 0; }
-
-   public int getHandSize() { return 0; }
-
-   public int getHandValue()
-   {
-		//great example of polymorphism
-      int total=0;
-
-
-
-
-
-
-
-      return total;
+   public  void setWinCount( int numwins ) {
+       winCount = numwins;
    }
 
-   public String toString()
-   {
-      return "hand = " + hand.toString() + " \n-  # wins " + winCount;
+   public int getWinCount() {return winCount;}
+
+   public int getHandSize() {return hand.size();}
+
+   public int getHandValue() {
+       int total = 0;
+       for (int i = 0; i < hand.size(); i++){
+           if (hand.get(i).getFace() == 1){
+               skip.add(i);
+           }
+           else{
+               total += hand.get(i).getValue();
+           }
+       }
+       for (int i = 0; i < skip.size(); i++){
+           if (total <= 10){
+               total += 11;
+           }
+           else{
+               total += 1;
+           }
+       }
+       skip.clear();
+       return total;
+   }
+
+   public String toString() {
+      return "hand = " + hand.toString() + "\n Hand value - " + getHandValue() + " \n Wins - " + winCount;
    }
 }
