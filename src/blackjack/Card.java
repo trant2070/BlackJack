527e6131c967ce4fc35c63ec0a9f21e197e0ab1d
trant2070 <31450029+trant2070@users.noreply.github.com>
2019-04-06 08:56:47 -0700

    Update Card.java

--- src/blackjack/Card.java
+++ src/blackjack/Card.java
@@ -7,39 +7,44 @@ package blackjack;
 //Lab  - 
 
 
-public abstract class Card
-{
-	public static final String FACES[] = {"ZERO","ACE","TWO","THREE","FOUR",
-			"FIVE","SIX","SEVEN","EIGHT","NINE","TEN","JACK","QUEEN","KING"};
-
+public abstract class Card {
+	public static final String FACES[] = {"ZERO", "ACE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE", "TEN", "JACK", "QUEEN", "KING"};
 	private String suit;
 	private int face;
 
-  	//constructors
+  	public Card() {
+            setFace(0);
+            setSuit("");
+        }
+        
+        public Card(int f, String s) {
+            setFace(f);
+            setSuit(s);
+        }
+        
+        public void setFace(int f) {
+            face = f;
+        }
+        
+        public void setSuit(String s) {
+            suit = s;
+        }
+        
+        public int getFace() {
+            return face;
+        }
+        
+        public String getSuit() {
+            return suit;
+        }
+        
+        public abstract int getValue();
 
-
-
-	// modifiers
-
-
- 
-
-  	//accessors
-
-
-
-
-
-  	public abstract int getValue();
-
-	public boolean equals(Object obj)
-	{
-
-
-
-		return false;
+	public boolean equals(Object obj) {
+            Card c = (Card)obj;
+            return c.getValue() == getValue() && c.getSuit().equals(getSuit());
 	}
-
-  	//toString
-  	
- }
+        
+        public String toString() {
+            return FACES[face] + " of " + getSuit() + " | value = " + getValue();
+        }
